c6707c2a72416e4b7125bab7e94fd0fa4b7f19b4
Kim Richards <richards.kimc@gmail.com>
2025-06-04 18:36:05 +0100

    Implemented a Retrieving Command Line Argument Example

---
+++ CSharpLogic/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace CSharpLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Retrieving the ProjectName dynamically
+            if (args.Length > 0)
+            {
+                string commandLineArgument = new(args[0]);
+                Console.WriteLine(commandLineArgument);
+            }
+            else
+            {
+                Console.WriteLine("No command line argument provided.");
+            }
+
+        }
+    }
+    
+    
+}
